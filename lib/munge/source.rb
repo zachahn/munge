6d9a0a08c7db6683200a9cefb60d6b2c053f9ed1
Zach Ahn <zach.ahn@gmail.com>
2015-09-10 01:21:38 -0400

    make Munge::Source act more like an enumerable

--- lib/munge/source.rb
+++ lib/munge/source.rb
@@ -1,14 +1,21 @@
 module Munge
   class Source
+    include Enumerable
+
     def initialize(source_abspath)
-      @pattern = File.join(source_abspath, "**", "*")
+      @source_abspath = source_abspath
+      @pattern        = File.join(source_abspath, "**", "*")
+
+      @items = Dir[@pattern]
+               .reject { |item_path| File.directory?(item_path) }
+               .map    { |item_path| Item.create(source_abspath, item_path) }
     end
 
     def each
       return enum_for(:each) unless block_given?
 
-      Dir.glob(@pattern) do |file|
-        yield file
+      @items.each do |item|
+        yield item
       end
     end
   end
