419314934053deae7b0fd8a7a9c7b8ece098c294
Zach Ahn <zach.ahn@gmail.com>
2015-09-10 01:21:12 -0400

    add content to item

--- lib/munge/item.rb
+++ lib/munge/item.rb
@@ -2,19 +2,21 @@ module Munge
   class Item
     def self.create(source_abspath, file_abspath)
       path     = Munge::Attribute::Path.new(source_abspath, file_abspath)
+      content  = Munge::Attribute::Content.new(File.read(file_abspath))
       metadata = Munge::Attribute::Metadata.new(file_abspath)
 
-      new(path, metadata)
+      new(path, content, metadata)
     end
 
-    def initialize(path, metadata)
+    def initialize(path, content, metadata)
       @path     = path
+      @content  = content
       @metadata = metadata
       @route    = nil
       @layout   = nil
     end
 
-    attr_reader :path, :metadata
+    attr_reader :path, :content, :metadata
     attr_accessor :route, :layout
   end
 end
