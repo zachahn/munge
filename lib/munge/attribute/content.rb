6412dd52ff66eb877669c29058008498047e415c
Zach Ahn <zach.ahn@gmail.com>
2015-09-17 18:19:38 -0400

    allow for Content's frontmatter to be set via args

--- lib/munge/attribute/content.rb
+++ lib/munge/attribute/content.rb
@@ -43,10 +43,14 @@ module Munge
         end
       end
 
-      def initialize(string)
+      def initialize(string, frontmatter = nil)
         @frontmatter, @content = self.class.parse(string)
+
+        if frontmatter
+          @frontmatter = @frontmatter.merge(frontmatter)
+        end
       rescue ArgumentError
-        @frontmatter = {}
+        @frontmatter = frontmatter || {}
         @content     = string
       end
 
