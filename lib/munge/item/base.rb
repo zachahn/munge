e24dbb7ed2ce9d983ae85ea917e949b6c7458665
Zach Ahn <zach.ahn@gmail.com>
2015-09-17 15:58:17 -0400

    create Item::Base and tests

---
+++ lib/munge/item/base.rb
@@ -0,0 +1,67 @@
+module Munge
+  module Item
+    class Base
+      def initialize(path = nil, content = nil, info = nil)
+        @path    = path
+        @content = content
+        @info    = info
+        @layout  = nil
+        @route   = nil
+      end
+
+      attr_accessor :route, :layout
+
+      def id
+        if dirname == ""
+          basename
+        else
+          "#{dirname}/#{basename}"
+        end
+      end
+
+      def relpath
+        @path.relative
+      end
+
+      def dirname
+        if File.dirname(relpath) == "."
+          ""
+        else
+          File.dirname(relpath)
+        end
+      end
+
+      def filename
+        File.basename(relpath)
+      end
+
+      def basename
+        filename.split(".").first
+      end
+
+      def content
+        @content.content
+      end
+
+      def frontmatter
+        @content.frontmatter
+      end
+
+      def stat
+        @info.stat
+      end
+
+      def virtual?
+        false
+      end
+
+      def binary?
+        false
+      end
+
+      def text?
+        false
+      end
+    end
+  end
+end
---
+++ test/item_base_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+require "tilt/erb"
+require "tilt/plain"
+
+class ItemBaseTest < Minitest::Test
+  def setup
+    fixtures = File.absolute_path(File.expand_path("../fixtures", __FILE__))
+
+    top_fixture = "#{fixtures}/test-item-interface.html.erb"
+    sub_fixture = "#{fixtures}/subdir/file.html.erb"
+
+    @top_item = Munge::Item::Base.new(
+      Munge::Attribute::Path.new(fixtures, top_fixture),
+      Munge::Attribute::Content.new(File.read(top_fixture)),
+      Munge::Attribute::Metadata.new(top_fixture)
+    )
+
+    @sub_item = Munge::Item::Base.new(
+      Munge::Attribute::Path.new(fixtures, sub_fixture),
+      Munge::Attribute::Content.new(File.read(sub_fixture)),
+      Munge::Attribute::Metadata.new(sub_fixture)
+    )
+  end
+
+  def test_item_type_is_base
+    assert_equal false, @top_item.virtual?
+    assert_equal false, @top_item.binary?
+    assert_equal false, @top_item.text?
+  end
+
+  def test_paths
+    assert_equal "subdir/file.html.erb", @sub_item.relpath
+    assert_equal "subdir", @sub_item.dirname
+    assert_equal "file.html.erb", @sub_item.filename
+    assert_equal "file", @sub_item.basename
+    assert_equal "subdir/file", @sub_item.id
+
+    assert_equal "test-item-interface.html.erb", @top_item.relpath
+    assert_equal "test-item-interface.html.erb", @top_item.filename
+    assert_equal "test-item-interface", @top_item.basename
+    assert_equal "", @top_item.dirname
+    assert_equal "test-item-interface", @top_item.id
+  end
+
+  def test_content_and_frontmatter
+    assert_equal %(super <%= "cool" %>\n), @sub_item.content
+    assert_equal({}, @sub_item.frontmatter)
+
+    assert_equal "love game", @top_item.frontmatter["song_name"]
+  end
+
+  def test_stat
+    assert_kind_of File::Stat, @top_item.stat
+  end
+end
