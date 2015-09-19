bafbcd192cfca4d7bbd04e7b63073876d47e5859
Zach Ahn <zach.ahn@gmail.com>
2015-09-19 19:04:17 -0400

    add super basic Application test

---
+++ test/application_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class ApplicationTest < Minitest::Test
+  def setup
+    example = File.expand_path("example", File.dirname(__FILE__))
+    
+    @application = Munge::Application.new("#{example}/config.yml")
+  end
+end
