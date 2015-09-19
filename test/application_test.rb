2d6c599cd57d145d4f1dbaaf9e61e1e7903c60c4
Zach Ahn <zach.ahn@gmail.com>
2015-09-19 19:09:04 -0400

    whitespace

--- test/application_test.rb
+++ test/application_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ApplicationTest < Minitest::Test
   def setup
     example = File.expand_path("example", File.dirname(__FILE__))
-    
+
     @application = Munge::Application.new("#{example}/config.yml")
   end
 end
