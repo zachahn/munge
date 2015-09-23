c396bf20d14d5b5fb11516ff970072f322625661
Zach Ahn <zach.ahn@gmail.com>
2015-09-23 00:53:17 -0400

    use `def foo(**arg)` instead of `def foo(arg = {})`

--- lib/munge/tilt_item_renderer.rb
+++ lib/munge/tilt_item_renderer.rb
@@ -5,7 +5,7 @@ module Munge
       @layouts_path = layouts_path
     end
 
-    def render(item, additional_data = {})
+    def render(item, **additional_data)
       ::Tilt.templates_for(item.relpath)
         .inject(item.content) do |output, engine|
           template = engine.new { output }
@@ -13,7 +13,7 @@ module Munge
         end
     end
 
-    def layout(path, additional_data = {}, &block)
+    def layout(path, **additional_data, &block)
       actual_layout = find_layout(path)
 
       if block.binding.local_variable_defined?(:_erbout)
@@ -23,7 +23,7 @@ module Munge
       end
     end
 
-    def render_with_layout(item, additional_data = {})
+    def render_with_layout(item, **additional_data)
       layout(item.layout, additional_data) do
         render(item, additional_data)
       end
