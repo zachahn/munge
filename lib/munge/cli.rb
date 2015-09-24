057d6009ba4a4dc24883b6cd5fdc7069b4b48780
Zach Ahn <zach.ahn@gmail.com>
2015-09-24 14:01:23 -0400

    define `munge init`

--- lib/munge/cli.rb
+++ lib/munge/cli.rb
@@ -2,6 +2,17 @@ require "thor"
 
 module Munge
   class CLI < Thor
+    include Thor::Actions
+
+    def self.source_root
+      File.expand_path("../../../seeds", __FILE__)
+    end
+
+    desc "init PATH", "Create new site at PATH"
+    def init(path)
+      directory ".", path
+    end
+
     desc "version", "Print version"
     def version
       puts "munge #{Munge::VERSION}"
