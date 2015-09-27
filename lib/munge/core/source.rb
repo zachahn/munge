eabf7adb13348564b1f8bfeac207767509fb714f
Zach Ahn <zach.ahn@gmail.com>
2015-09-27 00:22:24 -0400

    change source datastore to a hash

--- lib/munge/core/source.rb
+++ lib/munge/core/source.rb
@@ -9,21 +9,29 @@ module Munge
         item_factory = ItemFactory.new(source_abspath, binary_extensions)
         pattern      = File.join(source_abspath, "**", "*")
 
-        @items = Dir[pattern]
-                 .reject { |item_path| File.directory?(item_path) }
-                 .map    { |item_path| item_factory.create(item_path) }
+        @items =
+          Dir.glob(pattern)
+            .reject { |item_path| File.directory?(item_path) }
+            .map    { |item_path| item_factory.create(item_path) }
+            .map    { |item| [item.id, item] }
+            .to_h
       end
 
       def each
         return enum_for(:each) unless block_given?
 
-        @items.each do |item|
+        @items.each_value do |item|
           yield item
         end
       end
 
       def push(virtual_item)
-        @items.push(virtual_item)
+        key = virtual_item.id
+        @items[key] = virtual_item
+      end
+
+      def [](id)
+        @items[id]
       end
     end
   end
