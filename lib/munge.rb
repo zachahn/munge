617240723dcac543e6125e1c4ad6b53dd91f4346
Zach Ahn <zach.ahn@gmail.com>
2015-10-03 23:56:05 -0400

    ItemFactory

--- lib/munge.rb
+++ lib/munge.rb
@@ -1,5 +1,6 @@
 require "fileutils"
 require "pathname"
+require "set"
 require "yaml"
 
 require "tilt"
--- test/core__source__item_factory_test.rb
+++ test/core__source__item_factory_test.rb
@@ -2,22 +2,35 @@ require "test_helper"
 
 class CoreSourceItemFactoryTest < Minitest::Test
   def setup
-    @fixtures = File.absolute_path(File.expand_path("../fixtures", __FILE__))
-
     binary_extensions = %w(gif png)
+    ignored_basenames = %w(index dir)
 
-    @item_factory = Munge::ItemFactory.new(@fixtures, binary_extensions)
+    @item_factory = Munge::ItemFactory.new(source_path, binary_extensions, :fs_memory, ignored_basenames)
   end
 
-  def test_item_makes_binaries
-    item = @item_factory.create("#{@fixtures}/transparent.gif")
-
-    assert_kind_of Munge::Item::Binary, item
+  def test_item_read
+    item = @item_factory.read(File.join(source_path, "frontmatter.html.erb"))
+    assert_equal %(Guess he wants to play, a <%= song_name %>\n), item.content
+    assert_equal "love game", item["song_name"]
   end
 
-  def test_item_makes_texts
-    item = @item_factory.create("#{@fixtures}/subdir/file.html.erb")
+  def test_type
+    txt = @item_factory.read(File.join(source_path, "frontmatter.html.erb"))
+    assert_equal :text, txt.type
 
-    assert_kind_of Munge::Item::Text, item
+    bin = @item_factory.read(File.join(source_path, "transparent.gif"))
+    assert_equal :binary, bin.type
+    assert_equal Hash.new, bin.frontmatter
+  end
+
+  def test_id
+    item = @item_factory.read(File.join(source_path, "index.html.erb"))
+    assert_equal "", item.id
+
+    item = @item_factory.read(File.join(source_path, "render.html.erb"))
+    assert_equal "render", item.id
+
+    item = @item_factory.read(File.join(source_path, "in/sub/dir.html.erb"))
+    assert_equal "in/sub", item.id
   end
 end
