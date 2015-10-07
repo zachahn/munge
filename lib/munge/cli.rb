122a9a9a4f671a35bce617f0014bb939e383eb52
Zach Ahn <zach.ahn@gmail.com>
2015-10-07 15:43:27 -0400

    add `munge view` for viewing build directory

--- lib/munge/cli.rb
+++ lib/munge/cli.rb
@@ -18,6 +18,27 @@ module Munge
       Munge::Runner.write(config_path, rules_path)
     end
 
+    desc "view", "View built files"
+    method_option :port, aliases: "-p", desc: "Set port", default: 3000, type: :numeric
+    method_option :host, aliases: "-h", desc: "Set host", default: "0.0.0.0", type: :string
+    def view
+      config    = Munge::Core::Config.new(config_path)
+      handler   = Rack::Handler::WEBrick
+      rack_opts = { Host: options[:host], Port: options[:port] }
+
+      app =
+        Rack::Builder.new do
+          use Rack::CommonLogger
+          use Rack::ShowExceptions
+          use Rack::Lint
+          use Rack::Head
+          use Adsf::Rack::IndexFileFinder, root: config[:output]
+          run Rack::File.new(config[:output])
+        end
+
+      Rack::Handler::WEBrick.run(app, rack_opts)
+    end
+
     desc "version", "Print version"
     def version
       puts "munge #{Munge::VERSION}"
