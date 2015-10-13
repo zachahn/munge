c24b2a7b20a965c46dcb3c66f35e17a46dbe8681
Zach Ahn <zach.ahn@gmail.com>
2015-10-13 16:06:10 -0400

    keyword args for Scope

--- lib/munge/transformer/tilt/scope.rb
+++ lib/munge/transformer/tilt/scope.rb
@@ -2,16 +2,16 @@ module Munge
   module Transformer
     class Tilt
       class Scope
-        def initialize(source_path,
-                       layouts_path,
-                       global_data,
-                       source,
-                       router)
-          @source_path      = source_path
-          @layouts_path     = layouts_path
-          @global_data      = global_data
-          @source           = source
-          @router           = router
+        def initialize(source_path:,
+                       layouts_path:,
+                       global_data:,
+                       source:,
+                       router:)
+          @source_path  = source_path
+          @layouts_path = layouts_path
+          @global_data  = global_data
+          @source       = source
+          @router       = router
         end
       end
     end
