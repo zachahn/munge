7d571a1320b6db48fb5a5ec49f04fc07844aa786
Zach Ahn <zach.ahn@gmail.com>
2015-10-16 01:39:41 -0400

    have integration test also test layouts

--- test/application_test.rb
+++ test/application_test.rb
@@ -26,6 +26,7 @@ class ApplicationTest < Minitest::Test
       home_item = app.source[""]
       home_item.route = home_item.id
       home_item.transform
+      home_item.layout = "basic"
 
       tgif_item = app.source["transparent"]
       tgif_item.route = tgif_item.id
@@ -33,6 +34,11 @@ class ApplicationTest < Minitest::Test
       md_no_ext_item = app.source["md_no_ext"]
       md_no_ext_item.route = md_no_ext_item.id
       md_no_ext_item.transform(:tilt, "md")
+
+      cant_find_item = app.source["frontmatter_and_markdown"]
+      cant_find_item.route = "justdance.html"
+      cant_find_item.transform
+      cant_find_item.layout = "req_global_data"
 
       app.write
 
@@ -42,11 +48,13 @@ class ApplicationTest < Minitest::Test
       @transparent = File.read("#{output_path}/transparent.gif")
       @index       = File.read("#{output_path}/index.html")
       @md_no_ext   = File.read("#{output_path}/md_no_ext/index.html")
+      @justdance   = File.read("#{output_path}/justdance.html")
     end
 
     assert_equal "<h1>about me</h1>\n", @about
     assert_equal 37, @transparent.size
-    assert_equal "hi", @index
+    assert_equal "<body>hi</body>\n", @index
     assert_equal "<h1>hi</h1>\n", @md_no_ext
+    assert_equal "<body>this is a test<p><strong>cant find</strong> my drink or man</p>\n</body>\n", @justdance
   end
 end
