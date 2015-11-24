2204506fbac9628591e5cf0275f066a8223fb04b
Zach Ahn <zach.ahn@gmail.com>
2015-11-23 21:37:38 -0500

    rewrite Munge::Transformer::Tilt

--- lib/munge/transformer/tilt.rb
+++ lib/munge/transformer/tilt.rb
@@ -3,12 +3,26 @@ require_relative "tilt/scope"
 module Munge
   module Transformer
     class Tilt
-      def initialize(scope_factory)
-        @scope = scope_factory.create
+      def initialize(scope)
+        @pristine_scope = scope
+      end
+
+      def name
+        :tilt
       end
 
       def call(item, content = nil, renderer = nil)
-        @scope.render_with_layout(item, content_engines: renderer, content_override: content)
+        scope = @pristine_scope.dup
+        dirty_scope = extend_with_helpers(scope)
+        dirty_scope.render_with_layout(item, content_engines: renderer, content_override: content)
+      end
+
+      private
+
+      def extend_with_helpers(scope)
+        Munge::Helper.constants
+          .map  { |sym| Munge::Helper.const_get(sym) }
+          .inject(scope) { |scope, helper| scope.extend(helper) }
       end
     end
   end
