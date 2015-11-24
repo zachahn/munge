46b80d8a08e67ece4c960e8273fd3f4ff2985197
Zach Ahn <zach.ahn@gmail.com>
2015-11-24 17:49:51 -0500

    remove unused code

--- lib/munge/helper/rendering.rb
+++ lib/munge/helper/rendering.rb
@@ -15,16 +15,6 @@ module Munge
         mdata       = merged_data(layout_item.frontmatter, data)
 
         render_string(layout_item.content, data: mdata, engines: renderers, &block)
-
-        # if block_given?
-        #   if block.binding.local_variable_defined?(:_erbout)
-        #     layout_within_template(layout_item, mdata, &block)
-        #   else
-        #     layout_outside_template(layout_item, mdata, &block)
-        #   end
-        # else
-        #   layout_without_block(layout_item, mdata)
-        # end
       end
 
       def render_string(content, data: {}, engines: [])
@@ -82,32 +72,6 @@ module Munge
         end
       end
 
-      def layout_outside_template(layout_item, mdata, &block)
-        engine_list = tilt_renderer_list(layout_item, nil)
-
-        manual_render(layout_item.content, mdata, engine_list, &block)
-      end
-
-      def layout_within_template(layout_item, mdata, &block)
-        original_erbout = block.binding.local_variable_get(:_erbout)
-
-        block.binding.local_variable_set(:_erbout, "")
-
-        result = layout_outside_template(layout_item, mdata) { block.call }
-
-        final = original_erbout + result
-
-        block.binding.local_variable_set(:_erbout, final)
-
-        ""
-      end
-
-      def layout_without_block(layout_item, mdata)
-        engine_list = tilt_renderer_list(layout_item, nil)
-
-        manual_render(layout_item.content, mdata, engine_list)
-      end
-
       def tilt_renderer_list(item, preferred_engine)
         if preferred_engine
           tilt_renderers_from_preferred(preferred_engine)
@@ -129,26 +93,6 @@ module Munge
           end
 
         ::Tilt.templates_for(preferred)
-      end
-
-      def manual_render(content, data, engine_list, &block)
-        inner =
-          if block_given?
-            block.call
-          else
-            nil
-          end
-
-        engine_list
-          .inject(content) do |output, engine|
-            template = engine.new { output }
-
-            if inner
-              template.render(self, data) { inner }
-            else
-              template.render(self, data)
-            end
-          end
       end
     end
   end
