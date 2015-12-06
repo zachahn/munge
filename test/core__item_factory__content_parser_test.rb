efc11ac49ccc07bd8506b542e107b5e20aac75a9
Zach Ahn <zach.ahn@gmail.com>
2015-12-05 22:58:11 -0500

    add tests and fix ContentParser

--- test/core__item_factory__content_parser_test.rb
+++ test/core__item_factory__content_parser_test.rb
@@ -39,4 +39,17 @@ have you any wool)
     assert_equal("baa baa\n\n---\n\nblack sheep\n---\nhave you any wool",
       match.content)
   end
+
+  def test_content_parser_that_looks_like_frontmatter_but_really_isnt
+    match = Munge::Core::ItemFactory::ContentParser.new(
+      %(---
+not frontmatter
+---
+
+yup)
+    )
+
+    assert_equal({}, match.frontmatter)
+    assert_equal("yup", match.content)
+  end
 end
