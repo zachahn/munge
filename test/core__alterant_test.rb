a36068c79a85636e7cb6f5adb41de9b19c46ee1c
Zach Ahn <zach.ahn@gmail.com>
2015-12-10 23:22:53 -0500

    simplify test a little

--- test/core__alterant_test.rb
+++ test/core__alterant_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 class CoreAlterantTest < Minitest::Test
   def setup
     @alterant = Munge::Core::Alterant.new(scope: Object.new)
+  end
 
+  def register_rot13!
     @rot13 = Object.new
 
     def @rot13.name
@@ -14,6 +16,10 @@ class CoreAlterantTest < Minitest::Test
       content.tr("a-z", "n-za-m")
     end
 
+    @alterant.register(@rot13)
+  end
+
+  def retister_double!
     @double = Object.new
 
     def @double.name
@@ -23,16 +29,18 @@ class CoreAlterantTest < Minitest::Test
     def @double.call(item, content, *)
       content + content
     end
+
+    @alterant.register(@double)
   end
 
   def test_registration_disallows_transformers_with_same_name
-    @alterant.register(@rot13)
+    register_rot13!
 
     assert_raises(RuntimeError) { @alterant.register(@rot13) }
   end
 
   def test_transform
-    @alterant.register(@rot13)
+    register_rot13!
 
     item = OpenStruct.new
     item.transforms = [[:rot13]]
@@ -44,8 +52,8 @@ class CoreAlterantTest < Minitest::Test
   end
 
   def test_multiple_transforms
-    @alterant.register(@rot13)
-    @alterant.register(@double)
+    register_rot13!
+    retister_double!
 
     item = OpenStruct.new
     item.transforms = [[:rot13], [:double]]
