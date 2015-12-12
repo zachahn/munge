a3aa2ba40607dce1511e2f8b7e8695fe24984d56
Zach Ahn <zach.ahn@gmail.com>
2015-12-12 12:04:53 -0500

    create AutoAddExtension router
    - Adds extension to route if the route is missing extension
    - The list of extensions to append on is configurable

---
+++ lib/munge/routers/auto_add_extension.rb
@@ -0,0 +1,44 @@
+module Munge
+  module Router
+    class AutoAddExtension
+      def initialize(keep_extensions:)
+        @keep_extensions = keep_extensions
+      end
+
+      def match?(initial_route, content, item)
+        item_should_have_extension?(item) && route_doesnt_have_extension?(initial_route)
+      end
+
+      def route(initial_route, _content, item)
+        add_extension(initial_route, item)
+      end
+
+      def filepath(initial_route, _content, item)
+        add_extension(initial_route, item)
+      end
+
+      private
+
+      def add_extension(initial_route, item)
+        intersection = item.extensions & @keep_extensions
+        extension    = intersection[0]
+
+        "#{initial_route}.#{extension}"
+      end
+
+      def item_should_have_extension?(item)
+        intersection = item.extensions & @keep_extensions
+
+        intersection.length > 0
+      end
+
+      def route_doesnt_have_extension?(initial_route)
+        initial_route_extensions = initial_route.split(".")[1..-1]
+
+        intersection = initial_route_extensions & @keep_extensions
+
+        intersection.length == 0
+      end
+    end
+  end
+end
