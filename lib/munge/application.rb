5869179e9c3ddb899cad6f8feeb79f581c11f3f6
Zach Ahn <zach.ahn@gmail.com>
2016-01-03 12:31:11 -0500

    move setup from Application => setup.rb
    Only tested via integration test. I’ll fix breaking tests by avoiding
    the filesystem

--- lib/munge/application.rb
+++ lib/munge/application.rb
@@ -2,17 +2,6 @@ module Munge
   class Application
     def initialize(system)
       @system = system
-
-      tilt_transformer = Transformer::Tilt.new(@system)
-      tilt_transformer.register(Munge::Helper::Find)
-      tilt_transformer.register(Munge::Helper::Link)
-      tilt_transformer.register(Munge::Helper::Rendering)
-
-      @system.alterant.register(tilt_transformer)
-
-      @system.router.register(Router::AutoAddExtension.new(keep_extensions: system.config[:keep_extensions]))
-      @system.router.register(Router::Fingerprint.new(extensions: system.config[:keep_extensions]))
-      @system.router.register(Router::IndexHtml.new(html_extensions: system.config[:text_extensions], index: system.config[:index]))
     end
 
     def source
