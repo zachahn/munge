d37d75d233ab94a378e1331638e42a0555225eab
Zach Ahn <zach.ahn@gmail.com>
2016-01-03 17:31:34 -0500

    make the seed files slightly more realistic

--- seeds/rules.rb
+++ seeds/rules.rb
@@ -1,5 +1,11 @@
 app.source
-  .select { |item| item.id == "" }
-  .each   { |item| item.route = "/" }
+  .select { |item| item.extensions.include?("html") }
+  .each   { |item| item.route = item.basename }
   .each   { |item| item.layout = "default" }
+  .each   { |item| item.transform }
+
+app.source
+  .select { |item| item.extensions.include?("scss") }
+  .reject { |item| item.basename[0] == "_" }
+  .each   { |item| item.route = "/styles/#{item.basename}.css" }
   .each   { |item| item.transform }
