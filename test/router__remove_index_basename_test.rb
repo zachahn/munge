68fe93ccc1f9f0e6778bbd564ed9d9810cda4256
Zach Ahn <zach.ahn@gmail.com>
2016-01-06 14:21:12 -0500

    fix router that removes "index" basename

--- test/router__remove_index_basename_test.rb
+++ test/router__remove_index_basename_test.rb
@@ -13,18 +13,18 @@ class RouterRemoveIndexBasenameTest < Minitest::Test
     item = OpenStruct.new
     item.extensions = %w(html erb)
 
-    assert_equal true, @index_remover.match?("index", "", item)
-    assert_equal false, @index_remover.match?("about", "", item)
+    assert_equal true, @index_remover.match?("index", item)
+    assert_equal false, @index_remover.match?("about", item)
 
-    assert_equal false, @index_remover.match?("index.htm", "", item)
-    assert_equal false, @index_remover.match?("index.html", "", item)
-    assert_equal false, @index_remover.match?("about.html", "", item)
+    assert_equal false, @index_remover.match?("index.htm", item)
+    assert_equal false, @index_remover.match?("index.html", item)
+    assert_equal false, @index_remover.match?("about.html", item)
   end
 
-  def test_route
+  def test_call
     item = Object.new
 
-    assert_equal "", @index_remover.route("index", "", item)
-    assert_equal "about", @index_remover.route("about/index", "", item)
+    assert_equal "", @index_remover.call("index", item)
+    assert_equal "about", @index_remover.call("about/index", item)
   end
 end
