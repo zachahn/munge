5a6e27515be4ec3c1b44a8658ea4f310ee9795c5
Zach Ahn <zach.ahn@gmail.com>
2016-01-06 23:52:30 -0500

    add Capture helper to seeds

--- seeds/setup.rb
+++ seeds/setup.rb
@@ -2,6 +2,7 @@ require "sass"
 Sass.load_paths << File.expand_path(system.config[:source], File.dirname(__FILE__))
 
 tilt_transformer = Transformer::Tilt.new(system)
+tilt_transformer.register(Munge::Helper::Capture)
 tilt_transformer.register(Munge::Helper::Find)
 tilt_transformer.register(Munge::Helper::Link)
 tilt_transformer.register(Munge::Helper::Rendering)
