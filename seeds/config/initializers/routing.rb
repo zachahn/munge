e4a44319b6a2a6afc32a4232e85a837588f6e546
Zach Ahn <zach.ahn@gmail.com>
2016-01-11 02:06:30 -0500

    space-out router initializer better

--- seeds/config/initializers/routing.rb
+++ seeds/config/initializers/routing.rb
@@ -1,4 +1,19 @@
-system.router.register(Router::Fingerprint.new(extensions: config[:keep_extensions], separator: config[:fingeprint_separator]))
-system.router.register(Router::RemoveIndexBasename.new(html_extensions: config[:text_extensions], index: config[:index]))
-system.router.register(Router::AddIndexHtml.new(html_extensions: config[:text_extensions], index: config[:index]))
-system.router.register(Router::AutoAddExtension.new(keep_extensions: config[:keep_extensions]))
+system.router.register(
+  Router::Fingerprint.new(
+    extensions: config[:keep_extensions],
+    separator: config[:fingeprint_separator]
+  ))
+system.router.register(
+  Router::RemoveIndexBasename.new(
+    html_extensions: config[:text_extensions],
+    index: config[:index]
+  ))
+system.router.register(
+  Router::AddIndexHtml.new(
+    html_extensions: config[:text_extensions],
+    index: config[:index]
+  ))
+system.router.register(
+  Router::AutoAddExtension.new(
+    keep_extensions: config[:keep_extensions],
+  ))
