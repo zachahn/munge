02f64b45fda672557f6343f60f2ef623349f1ce8
Zach Ahn <zach.ahn@gmail.com>
2016-01-14 01:23:38 -0500

    create Sass functions for assets management

---
+++ seeds/config/sass_helpers.rb
@@ -0,0 +1,59 @@
+module Sass::Script::Functions
+  include AssetRoots
+end
+
+Sass::Script::Functions.send(:define_method, :route) do |id_ruby_string|
+  item = system.source[id_ruby_string]
+  r = system.router.route(item)
+
+  quoted_string(r)
+end
+
+Sass::Script::Functions.send(:define_method, :font_route) do |basename|
+  basename_string =
+    if basename.instance_of?(::String)
+      basename
+    else
+      basename.value
+    end
+
+  route("#{fonts_root}/#{basename_string}")
+end
+
+Sass::Script::Functions.send(:define_method, :image_route) do |basename|
+  basename_string =
+    if basename.instance_of?(::String)
+      basename
+    else
+      basename.value
+    end
+
+  route("#{images_root}/#{basename_string}")
+end
+
+Sass::Script::Functions.send(:define_method, :font_url) do |basename|
+  basename_string =
+    if basename.instance_of?(::String)
+      basename
+    else
+      basename.value
+    end
+
+  if basename_string.include?("?")
+    basename_parts = basename_string.split("?")
+
+    base = "#{basename_parts[0]}"
+    qs   = "?#{basename_parts[1]}"
+
+    path_to_font = quoted_string(font_route(base).value + qs)
+
+    unquoted_string("url(#{path_to_font})")
+  else
+    unquoted_string("url(#{font_route(basename)})")
+  end
+end
+
+Sass::Script::Functions.send(:define_method, :image_url) do |basename|
+  img_route = image_route(basename)
+  unquoted_string("url(#{img_route})")
+end
