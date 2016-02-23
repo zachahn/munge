7980f96c00ea8535b7e90b875717a4d9a23ea456
Zach Ahn <zach.ahn@gmail.com>
2016-02-22 21:14:48 -0500

    Rename methods and classes appropriately

--- test/helper__asset_paths_test.rb
+++ test/helper__asset_paths_test.rb
@@ -1,42 +1,42 @@
 require "test_helper"
 
-class HelperAssetUrlsTest < Minitest::Test
+class HelperAssetPathsTest < Minitest::Test
   def setup
     @renderer = QuickDummy.new(
       items: -> { Hash.new { |_h, k| "#{k}_item" } },
       url_for: -> (root) { "url/for/#{root}" }
     )
-    @renderer.extend(Munge::Helper::AssetUrls)
+    @renderer.extend(Munge::Helper::AssetPaths)
   end
 
-  def test_image_url
+  def test_image_path
     @renderer.define_singleton_method(:images_root) { "img" }
 
-    url = @renderer.image_url("basename")
+    url = @renderer.image_path("basename")
 
     assert_equal "url/for/img/basename_item", url
   end
 
-  def test_font_url
+  def test_font_path
     @renderer.define_singleton_method(:fonts_root) { "otf" }
 
-    url = @renderer.font_url("basename")
+    url = @renderer.font_path("basename")
 
     assert_equal "url/for/otf/basename_item", url
   end
 
-  def test_stylesheet_url
+  def test_stylesheet_path
     @renderer.define_singleton_method(:stylesheets_root) { "css" }
 
-    url = @renderer.stylesheet_url("basename")
+    url = @renderer.stylesheet_path("basename")
 
     assert_equal "url/for/css/basename_item", url
   end
 
-  def test_javascript_url
+  def test_javascript_path
     @renderer.define_singleton_method(:javascripts_root) { "js" }
 
-    url = @renderer.javascript_url("basename")
+    url = @renderer.javascript_path("basename")
 
     assert_equal "url/for/js/basename_item", url
   end
