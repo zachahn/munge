a2428cd678ebb3b62d5f9df496708e1fc8a42d9e
Zach Ahn <zach.ahn@gmail.com>
2016-03-05 19:08:16 -0500

    move some CLI stuff around

--- lib/munge/cli/commands/view.rb
+++ lib/munge/cli/commands/view.rb
@@ -25,21 +25,23 @@ require "adsf"
 require "rack"
 
 module Munge
-  class Commands
-    class View
-      def initialize(options, config_path)
-        config    = Munge::Util::Config.read(config_path)
-        rack_opts = { Host: options[:host], Port: options[:port] }
+  module Cli
+    module Commands
+      class View
+        def initialize(options, config_path)
+          config    = Munge::Util::Config.read(config_path)
+          rack_opts = { Host: options[:host], Port: options[:port] }
 
-        app =
-          Rack::Builder.new do
-            use Rack::ShowExceptions
-            use Rack::Head
-            use Adsf::Rack::IndexFileFinder, root: config[:output]
-            run Rack::File.new(config[:output])
-          end
+          app =
+            Rack::Builder.new do
+              use Rack::ShowExceptions
+              use Rack::Head
+              use Adsf::Rack::IndexFileFinder, root: config[:output]
+              run Rack::File.new(config[:output])
+            end
 
-        Rack::Handler::WEBrick.run(app, rack_opts)
+          Rack::Handler::WEBrick.run(app, rack_opts)
+        end
       end
     end
   end
---
+++ lib/munge/cli/dispatch.rb
@@ -0,0 +1,46 @@
+require "thor"
+
+module Munge
+  module Cli
+    class Dispatch < Thor
+      include Thor::Actions
+
+      def self.source_root
+        File.expand_path("../../../seeds", __FILE__)
+      end
+
+      desc "init PATH", "Create new site at PATH"
+      def init(path)
+        directory ".", path
+      end
+
+      desc "build", "Build in current directory"
+      def build
+        Munge::Runner.write(destination_root)
+      end
+
+      desc "view", "View built files"
+      method_option :port, aliases: "-p", desc: "Set port", default: 7000, type: :numeric
+      method_option :host, aliases: "-h", desc: "Set host", default: "0.0.0.0", type: :string
+      def view
+        require_relative "commands/view"
+        Commands::View.new(options, config_path)
+      end
+
+      desc "version", "Print version"
+      def version
+        puts "munge #{Munge::VERSION}"
+      end
+
+      private
+
+      def config_path
+        File.join(destination_root, "config.yml")
+      end
+
+      def rules_path
+        File.join(destination_root, "rules.rb")
+      end
+    end
+  end
+end
