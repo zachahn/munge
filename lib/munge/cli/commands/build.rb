fc9619d9be10dadeb62cd0281f449ec97f4da9b1
Zach Ahn <zach.ahn@gmail.com>
2016-03-19 17:28:29 -0400

    initialize the writer in commands/build

--- lib/munge/cli/commands/build.rb
+++ lib/munge/cli/commands/build.rb
@@ -3,15 +3,16 @@ module Munge
     module Commands
       class Build
         def initialize(destination_root, config, options)
-          app = application(destination_root)
+          @app = application(destination_root)
+          @dry_run = options[:dry_run]
           reporter_class = Munge::Reporters.const_get(options[:reporter])
 
           runner =
             Munge::Runner.new(
-              source: app.vomit(:source),
-              router: app.vomit(:router),
-              alterant: app.vomit(:alterant),
-              writer: app.vomit(:writer),
+              source: @app.vomit(:source),
+              router: @app.vomit(:router),
+              alterant: @app.vomit(:alterant),
+              writer: writer,
               reporter: reporter_class.new,
               destination: File.expand_path(config[:output], destination_root)
             )
@@ -25,6 +26,14 @@ module Munge
           bootstrap = Munge::Bootstrap.new_from_dir(root_path: root_path)
 
           bootstrap.app
+        end
+
+        def writer
+          if @dry_run
+            Munge::Writers::Noop.new
+          else
+            Munge::System::Write.new(output: "todo: delete")
+          end
         end
       end
     end
