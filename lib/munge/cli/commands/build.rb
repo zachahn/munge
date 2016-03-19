70073520bf66061bccdcb95c82f0e55a6c857041
Zach Ahn <zach.ahn@gmail.com>
2016-03-19 15:20:40 -0400

    Use const_get

--- lib/munge/cli/commands/build.rb
+++ lib/munge/cli/commands/build.rb
@@ -4,13 +4,15 @@ module Munge
       class Build
         def initialize(destination_root, options)
           app = application(destination_root)
+          reporter_class = Munge::Reporters.const_get(options[:reporter])
 
           runner =
             Munge::Runner.new(
               source: app.vomit(:source),
               router: app.vomit(:router),
               alterant: app.vomit(:alterant),
-              writer: app.vomit(:writer)
+              writer: app.vomit(:writer),
+              reporter: reporter_class.new
             )
 
           runner.write
