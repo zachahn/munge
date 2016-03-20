eb9fce6a934caa3a46dd426b0772aea3ed8dafe0
Zach Ahn <zach.ahn@gmail.com>
2016-03-19 21:31:43 -0400

    rubocop the seeds

--- seeds/config/sass.rb
+++ seeds/config/sass.rb
@@ -1,7 +1,7 @@
 require "munge/go/sass"
 
-Munge::Go::add_sass_load_path!(root_path, config[:source], AssetRoots.stylesheets_root)
+Munge::Go.add_sass_load_path!(root_path, config[:source], AssetRoots.stylesheets_root)
 
-Munge::Go::set_sass_system!(system)
+Munge::Go.set_sass_system!(system)
 
-Munge::Go::add_sass_functions!(AssetRoots)
+Munge::Go.add_sass_functions!(AssetRoots)
