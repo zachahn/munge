feaaaadd5f19e6a7836c101fde8b6324ec48a76e
Zach Ahn <zach.ahn@gmail.com>
2016-05-15 16:52:45 -0400

    Only fingerprint assets if MUNGE_ENV is production

--- seeds/config/routing.rb
+++ seeds/config/routing.rb
@@ -1,18 +1,23 @@
-system.router.register(
-  Routers::Fingerprint.new(
-    extensions: config[:bin_extensions] + config[:bintext_extensions],
-    separator: config[:fingeprint_separator]
-  ))
+if ENV["MUNGE_ENV"] == "production"
+  system.router.register(
+    Routers::Fingerprint.new(
+      extensions: config[:bin_extensions] + config[:bintext_extensions],
+      separator: config[:fingeprint_separator]
+    ))
+end
+
 system.router.register(
   Routers::RemoveIndexBasename.new(
     html_extensions: config[:text_extensions],
     index: config[:index]
   ))
+
 system.router.register(
   Routers::AddIndexHtml.new(
     html_extensions: config[:text_extensions],
     index: config[:index]
   ))
+
 system.router.register(
   Routers::AutoAddExtension.new(
     keep_extensions: config[:bin_extensions] + config[:bintext_extensions]
