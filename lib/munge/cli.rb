5d5319cb8a03688713d09bbe2588ae5cb88130cc
Zach Ahn <zach.ahn@gmail.com>
2016-06-02 22:26:11 -0400

    require Cli::Dispatch last

--- lib/munge/cli.rb
+++ lib/munge/cli.rb
@@ -1,9 +1,9 @@
 require "rainbow"
 
-require "munge/cli/dispatch"
 require "munge/cli/commands/build"
 require "munge/cli/commands/view"
 require "munge/cli/commands/server"
+require "munge/cli/dispatch"
 
 require "munge/formatters/default"
 require "munge/formatters/dots"
