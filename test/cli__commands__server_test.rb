82322f6ecbf4b0e4a8433499f744a0aadc7aa544
Zach Ahn <zach.ahn@gmail.com>
2016-06-02 23:20:42 -0400

    Add test for Commands::Server

---
+++ test/cli__commands__server_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class CliComandsServerTest < TestCase
+  include CommandInterfaceTest
+
+  test "#initialize" do
+    command
+  end
+
+  private
+
+  def bootloader
+    Munge::Bootloader.new(root_path: seeds_path)
+  end
+
+  def command
+    command_class.new(bootloader)
+  end
+
+  def command_class
+    Munge::Cli::Commands::Server
+  end
+end
