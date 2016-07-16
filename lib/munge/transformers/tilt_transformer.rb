cc4460daedfa1dd8215f044b157a8ba6a86c0176
Zach Ahn <zach.ahn@gmail.com>
2016-07-15 22:54:33 -0400

    Remove unneeded code

--- lib/munge/transformers/tilt_transformer.rb
+++ lib/munge/transformers/tilt_transformer.rb
@@ -13,7 +13,6 @@ module Munge
 
       def call(item, content = nil, renderer = nil)
         scope = @pristine_scope.dup
-        scope.instance_variable_set(:@renderer, @renderer)
         dirty_scope = extend_with_helpers(scope)
         dirty_scope.instance_variable_set(:@tilt_options, @demands)
         dirty_scope.render_with_layout(item, content_engines: renderer, content_override: content)
