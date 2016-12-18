023c97b7a27eefda773cf2392d4445dbc7a789bf
Zach Ahn <zach.ahn@gmail.com>
2016-12-17 23:29:53 -0500

    Lint some code

--- seeds/lib/routing.rb
+++ seeds/lib/routing.rb
@@ -3,22 +3,26 @@ if ENV["MUNGE_ENV"] == "production"
     Routers::Fingerprint.new(
       extensions: config[:bin_extensions] + config[:bintext_extensions],
       separator: config[:fingeprint_separator]
-    ))
+    )
+  )
 end
 
 system.router.register(
   Routers::RemoveIndexBasename.new(
     html_extensions: config[:text_extensions],
     index: config[:index]
-  ))
+  )
+)
 
 system.router.register(
   Routers::AddIndexHtml.new(
     html_extensions: config[:text_extensions],
     index: config[:index]
-  ))
+  )
+)
 
 system.router.register(
   Routers::AutoAddExtension.new(
     keep_extensions: config[:bin_extensions] + config[:bintext_extensions]
-  ))
+  )
+)
