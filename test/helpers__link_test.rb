35940e9a661bb696a9b0905c4c9b5832ed9019da
Zach Ahn <zach.ahn@gmail.com>
2016-12-23 14:51:51 -0500

    Make the Helpers::Link tests a bit more thorough
    I'm not sure if the tests are a bit too complicated now, but this now
    tests the full behavior more thoroughly. It catches the bug found in
    `HEAD~1`

--- test/helpers__link_test.rb
+++ test/helpers__link_test.rb
@@ -3,14 +3,17 @@ require "test_helper"
 class HelpersLinkTest < TestCase
   def setup
     dummy_router = Object.new
-
-    def dummy_router.route(*)
-      "/super/cool"
+    dummy_router.define_singleton_method(:route) do |obj|
+      if obj.respond_to?(:route)
+        "/object/#{obj.route}"
+      else
+        "/string/#{obj}"
+      end
     end
 
     system = Object.new
     system.define_singleton_method(:router) { dummy_router }
-    system.define_singleton_method(:items) { {} }
+    system.define_singleton_method(:items) { Hash.new { |_hash, key| key } }
 
     @renderer = tilt_scope_class.new(system, {})
     @renderer.extend(Munge::Helpers::Link)
@@ -18,38 +21,38 @@ class HelpersLinkTest < TestCase
   end
 
   test "#path_to" do
-    url = @renderer.path_to(Object.new)
+    url = @renderer.path_to(OpenStruct.new(route: "cool"))
 
-    assert_equal("/super/cool", url)
+    assert_equal("/object/cool", url)
   end
 
   test "#path_to works when a string is passed in" do
-    url = @renderer.path_to("")
+    url = @renderer.path_to("cool")
 
-    assert_equal("/super/cool", url)
+    assert_equal("/string/cool", url)
   end
 
   test "#link_to with custom text" do
-    html = @renderer.link_to(Object.new, "custom text")
+    html = @renderer.link_to(OpenStruct.new(route: "cool"), "custom text")
 
-    assert_equal(%(<a href="/super/cool">custom text</a>), html)
+    assert_equal(%(<a href="/object/cool">custom text</a>), html)
   end
 
   test "#link_to without custom text" do
-    html = @renderer.link_to(Object.new)
+    html = @renderer.link_to(OpenStruct.new(route: "cool"))
 
-    assert_equal(%(<a href="/super/cool">/super/cool</a>), html)
+    assert_equal(%(<a href="/object/cool">/object/cool</a>), html)
   end
 
   test "#link_to with html options/attributes" do
-    html = @renderer.link_to(Object.new, class: "hi", "asdf" => 3)
+    html = @renderer.link_to(OpenStruct.new(route: "cool"), class: "hi", "asdf" => 3)
 
-    assert_equal(%(<a href="/super/cool" class="hi" asdf="3">/super/cool</a>), html)
+    assert_equal(%(<a href="/object/cool" class="hi" asdf="3">/object/cool</a>), html)
   end
 
   test "#link_to with href option results in that option overriding default" do
-    html = @renderer.link_to(Object.new, href: "/hi", class: "hi")
+    html = @renderer.link_to(OpenStruct.new(route: "cool"), href: "/hi", class: "hi")
 
-    assert_equal(%(<a href="/hi" class="hi">/super/cool</a>), html)
+    assert_equal(%(<a href="/hi" class="hi">/object/cool</a>), html)
   end
 end
