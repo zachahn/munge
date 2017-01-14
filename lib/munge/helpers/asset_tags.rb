zachahn/munge

commit 8a75b8868c9c7d8f4d0a3d3b535f10df7829558f

Refactor asset tags' asset paths

lib/munge/helpers/asset_tags.rb
@@ -22,17 +22,17 @@ module Munge
       end
 
       def inline_stylesheet_tag(basename, options = {})
-        inline_asset_tag_helper(stylesheets_root, basename, :style, options)
+        inline_asset_tag_helper(:stylesheet_path, basename, :style, options)
       end
 
       def inline_javascript_tag(basename, options = {})
-        inline_asset_tag_helper(javascripts_root, basename, :script, options)
+        inline_asset_tag_helper(:javascript_path, basename, :script, options)
       end
 
       private
 
-      def inline_asset_tag_helper(root, basename, tag, options)
-        rendered_asset = render(items["#{root}/#{basename}"])
+      def inline_asset_tag_helper(asset_path_method, basename, tag, options)
+        rendered_asset = render(send(asset_path_method, basename))
 
         content_tag(tag, rendered_asset, options)
       end

test/helpers__asset_tags_test.rb
@@ -7,9 +7,6 @@ class HelpersAssetTagsTest < TestCase
         stylesheet_path: -> (basename) { "#{basename}.css" },
         javascript_path: -> (basename) { "#{basename}.js" },
         image_path: -> (basename) { basename },
-        stylesheets_root: -> { "stylesheets" },
-        javascripts_root: -> { "javascripts" },
-        images_root: -> { "images" },
         items: -> { Hash.new("item".freeze) },
         render: -> (_) { "rendered item" }
       )

commit eb179c8294be6444ce3047edda3107aa864267ec

Fix inline asset tags
This fixes a bug introduced by 8a75b8868c9c7d8f4d0a3d3b535f10df7829558f

This basically undoes that commit

lib/munge/helpers/asset_tags.rb
@@ -22,17 +22,18 @@ module Munge
       end
 
       def inline_stylesheet_tag(basename, options = {})
-        inline_asset_tag_helper(:stylesheet_path, basename, :style, options)
+        inline_asset_tag_helper(stylesheets_root, basename, :style, options)
       end
 
       def inline_javascript_tag(basename, options = {})
-        inline_asset_tag_helper(:javascript_path, basename, :script, options)
+        inline_asset_tag_helper(javascripts_root, basename, :script, options)
       end
 
       private
 
-      def inline_asset_tag_helper(asset_path_method, basename, tag, options)
-        rendered_asset = render(send(asset_path_method, basename))
+      def inline_asset_tag_helper(asset_root, basename, tag, options)
+        path_to_asset = File.join(asset_root, basename)
+        rendered_asset = render(items[path_to_asset])
 
         content_tag(tag, rendered_asset, options)
       end

test/helpers__asset_tags_test.rb
@@ -50,6 +50,8 @@ class HelpersAssetTagsTest < TestCase
       QuickDummy.new(
         stylesheet_path: -> (basename) { "#{basename}.css" },
         javascript_path: -> (basename) { "#{basename}.js" },
+        stylesheets_root: -> { "path/to/css" },
+        javascripts_root: -> { "path/to/js" },
         image_path: -> (basename) { basename },
         items: -> { Hash.new("item".freeze) },
         render: -> (_) { "rendered item" }