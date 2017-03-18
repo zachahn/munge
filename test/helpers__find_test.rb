4582378844a73aa33980f4da6842080e533cf901
Zach Ahn <engineering@zachahn.com>
2017-03-18 00:24:02 -0400

    Update view helper tests for #items and #layouts

--- test/helpers__find_test.rb
+++ test/helpers__find_test.rb
@@ -1,19 +1,27 @@
 require "test_helper"
 
 class HelpersFindTest < TestCase
-  test "#find" do
+  test "#items" do
     item = new_renderer.items[:id]
 
-    assert_equal("item", item)
+    assert_equal("an item", item)
+  end
+
+  test "#layouts" do
+    item = new_renderer.layouts[:id]
+
+    assert_equal("a layout", item)
   end
 
   private
 
   def new_renderer
-    items = { id: "item" }
+    items = { id: "an item" }
+    layouts = { id: "a layout" }
 
     system = Object.new
     system.define_singleton_method(:items) { items }
+    system.define_singleton_method(:layouts) { layouts }
 
     renderer = tilt_scope_class.new(system, {})
     renderer.extend(Munge::Helpers::Find)
